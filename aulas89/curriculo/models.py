1e21278bdd9fc2416f3d20c8d62d02bc1c73e059
Gilda Menezes <menezes.s.gilda@gmail.com>
2020-04-26 00:53:40 -0300

    create models for curriculo

--- aulas89/curriculo/models.py
+++ aulas89/curriculo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Curso(models.Model):
+    nome = models.CharField("Nome", max_length=50)
+    sigla = models.CharField("Sigla",max_length=5)
+    etiqueta = models.SlugField("Etiqueta", max_length=50)
+
+def __str__(self):
+        return self.nome
+
+class Disciplina(models.Model):
+    nome = models.CharField("Nome", max_length=50)
+    etiqueta = models.SlugField("Etiqueta", max_length=50)
+    carga_horaria = models.IntegerField("Carga Horaria")
+
+def __str__(self):
+    return self.nome
+    
+     
+
 # Create your models here.
